ab4ccd208cf872b2f7241b9e59f162fd0f6f2885
Shirley <ma_shirley@msn.com>
2017-02-01 20:45:03 -0800

    header.

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+
 $("form").submit(function(){
     alert("Thanks for subscribing");
 });
+
+$('.list-products').flickity({
+  // options
+   wrapAround: true,
+   freeScroll: true,
+   lazyLoad: 3
+});
+
