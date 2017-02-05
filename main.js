e3510f98466ab147865bc42bd54dbb9f625da12a
Shirley <ma_shirley@msn.com>
2017-02-05 14:47:35 -0800

    flickity installed.

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
 
+ $(function() {
+
 
 $("form").submit(function(){
     alert("Thanks for subscribing");
 });
 
-$('.list-products').flickity({
-  // options
-   wrapAround: true,
-   freeScroll: true,
-   lazyLoad: 3
-});
+
+//items
+   
+
+        
+        $('.list-products').flickity({
+        cellAlign: 'center',
+        contain: true,
+        percentPosition: true,
+        imagesLoaded: true,
+        autoPlay: true,
+        prevNextButtons: false
+    });
+
+
+//smooth scrolling
+
+// function pageJump(id) {
+//        $('html,body').animate({
+//                  scrollTop: $(id).offset().top - 150
+
+//          }, 'slow');
+// };
+// $('.navlink_names').on('click', function(e) {
+//           e.preventDefault();
+//                 var hashHref = $(this).attr('href');
+
+//                 pageJump(hashHref);
+// });
+
+
+
+    });
+    
+
+
+
+
+
 
